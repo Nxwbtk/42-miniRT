873e4186e7ba673f68dad9ba28d5943e42b54550
New Dev <65420147@kmitl.ac.th>
2023-10-01 05:59:41 +0700

    add validate input

--- main.c
+++ main.c
@@ -6,16 +6,44 @@
 /*   By: bsirikam <bsirikam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 01:21:10 by bsirikam          #+#    #+#             */
-/*   Updated: 2023/10/01 05:29:28 by bsirikam         ###   ########.fr       */
+/*   Updated: 2023/10/01 05:59:08 by bsirikam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
+int	ft_check(char *str)
+{
+	char	**split;
+	int		i;
+
+	split = NULL;
+	i = 0;
+	split = ft_split(str, '.');
+	while (split[i])
+		i++;
+	if (i != 2)
+		return (0);
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+	return (1);
+}
+
+void	run(char *filename)
+{
+}
+
 int	main(int ac, char **av)
 {
 	if (ac != 2)
 		return (1);
-	printf("%d\n", open(av[1], O_RDONLY));
+	if (!ft_check(av[1]))
+		return (1);
+	run(av[1]);
 	return (0);
 }
