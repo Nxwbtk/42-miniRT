dfabf6260adf557a3eacd62a25a2cb4518b0ad1f
Kittinon Saelim <ksaelim@z3t4c2.42bangkok.com>
2023-11-20 00:56:10 +0700

    vector tools, free & debug_print

--- src/utils/free.c
+++ src/utils/free.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   debug_vector.c                                     :+:      :+:    :+:   */
+/*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ksaelim <ksaelim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/20 00:51:51 by ksaelim           #+#    #+#             */
-/*   Updated: 2023/11/20 00:52:29 by ksaelim          ###   ########.fr       */
+/*   Created: 2023/11/20 00:50:17 by ksaelim           #+#    #+#             */
+/*   Updated: 2023/11/20 00:51:17 by ksaelim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-t_vec3f print_vec3f(char *point_name, t_vec3f vec)
+void	vec3f_free(t_vec3f *vec)
 {
-	printf("%s (%f, %f, %f)\n", point_name, vec.x, vec.y, vec.z);
-	return (vec);
+	if (vec->normalize)
+		free(vec->normalize);
 }
--- src/utils/vec_operator.c
+++ src/utils/vec_operator.c
@@ -6,11 +6,11 @@
 /*   By: ksaelim <ksaelim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 17:45:40 by ksaelim           #+#    #+#             */
-/*   Updated: 2023/10/19 10:22:38 by ksaelim          ###   ########.fr       */
+/*   Updated: 2023/11/20 00:52:13 by ksaelim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parsing.h"
+#include "miniRT.h"
 
 t_vec3f   new_vec3f(float x, float y, float z)
 {
@@ -20,8 +20,20 @@ t_vec3f   new_vec3f(float x, float y, float z)
 	vec.y = y;
 	vec.z = z;
 	vec.length = vec3f_length(vec);
-	vec.normalize = vec3f_div(vec, vec.length);
+	vec.normalize = vec3f_normalize(vec);
 	return (vec);
+}
+t_vec3f   *vec3f_normalize(t_vec3f vec)
+{
+	t_vec3f   *normalize;
+
+	normalize = malloc(sizeof(t_vec3f));
+	normalize->x = vec.x / vec.length;
+	normalize->y = vec.y / vec.length;
+	normalize->z = vec.z / vec.length;
+	normalize->length = 1;
+	normalize->normalize = NULL;
+	return (normalize);
 }
 
 t_vec3f	vec3f_add(t_vec3f vec1, t_vec3f vec2)
@@ -67,12 +79,4 @@ t_vec3f	vec3f_cross(t_vec3f vec1, t_vec3f vec2)
 	result.y = vec1.z * vec2.x - vec1.x * vec2.z;
 	result.z = vec1.x * vec2.y - vec1.y * vec2.x;
 	return (result);
-}
-
-#include <stdio.h>
-
-t_vec3f print_vec3f(char *point_name, t_vec3f vec)
-{
-	printf("%s (%f, %f, %f)\n", point_name, vec.x, vec.y, vec.z);
-	return (vec);
 }
