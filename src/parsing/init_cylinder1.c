f7688064dceb8d94ffe82f655c6328d507e096fb
bsirikam <buntakan0703@gmail.com>
2023-11-21 02:40:32 +0700

    Update cylinder color attribute

--- src/parsing/init_cylinder1.c
+++ src/parsing/init_cylinder1.c
@@ -6,7 +6,7 @@
 /*   By: buntakansirikamonthip <buntakansirikamonth +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 02:28:26 by bsirikam          #+#    #+#             */
-/*   Updated: 2023/11/21 02:39:28 by buntakansirikamo ###   ########.fr       */
+/*   Updated: 2023/11/21 02:40:20 by buntakansirikamo ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,6 @@ t_cy	*new_cylinder(char **split)
 	cylinder->dir = put_vec3f(split[2]);
 	cylinder->diameter = ft_atof(split[3]);
 	cylinder->height = ft_atof(split[4]);
-	cylinder->rgb = put_rgb(split[5]);
+	(*cylinder).clr = put_rgb(split[5]);
 	return (cylinder);
 }
