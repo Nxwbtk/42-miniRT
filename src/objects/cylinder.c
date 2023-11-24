5eade64abd570864171c4c1ee096fd9451114dcd
New Dev <65420147@kmitl.ac.th>
2023-11-25 05:10:22 +0700

    Fix hit_cylinder function to include body intersection

--- src/objects/cylinder.c
+++ src/objects/cylinder.c
@@ -6,11 +6,24 @@
 /*   By: bsirikam <bsirikam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 02:23:20 by bsirikam          #+#    #+#             */
-/*   Updated: 2023/11/25 03:54:52 by bsirikam         ###   ########.fr       */
+/*   Updated: 2023/11/25 05:10:02 by bsirikam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+
+int	hit_body(t_ray *ray, float t, t_cy *cylinder, float *dot)
+{
+	t_cor	point;
+	t_cor	origin_diff;
+
+	point = vec_add(ray->oringin, vec_multi_scalar(ray->dir, t));
+	origin_diff = vec_sub(point, cylinder->origin);
+	*dot = vec_dot_product(origin_diff, cylinder->dir);
+	if (fabs(*dot) > cylinder->height / 2)
+		return (0);
+	return (1);
+}
 
 int	is_cylinder(t_ray *ray, t_cy *cylinder)
 {
@@ -32,6 +45,9 @@ int	is_cylinder(t_ray *ray, t_cy *cylinder)
 	t[0] = t[1];
 	if (t[1] > t[2])
 		t[0] = t[2];
+	if (t[0] < 0.0 && t[0] <= ray->t && hit_body(ray, t[0], cylinder, &dot[0]))
+		return (1);
+	dot[1] = ray->t;
 }
 
 void	hit_cylinder(t_ray *ray, t_cy *cylinder, t_hitpoint *hitPoint)
